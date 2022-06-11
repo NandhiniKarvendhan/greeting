0463feccdb51044784b118f8442da957c447b479
NandhiniKarvendhan <NandhiniKarvendhan@Karvendhans-Mac-mini.local>
2022-06-11 16:57:57 +0530

    correcting the error of: Cannot find module course-utilities

--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,4 @@
-var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+var hello = require('./hello');
 
 test('outputs the correct string', () => {
   expect(hello()).toBe("Hello World!");
